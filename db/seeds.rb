ed8c4857b3ef6bbd85e6c1c44e0f9d4a632604fa
slzander <staceyzander@gmail.com>
2022-01-13 21:56:22 -0700

    Update seed file for easier manual testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,24 @@ tina = User.create(
   email: 'tina@guild.com'
 )
 
-105.times {
-  Message.create(sender: tina, recipient: fred, body: Faker::Lorem.sentence)
-}
+joe = User.create(
+  name: 'Joe',
+  email: 'joe@guild.com'
+)
+
+# Joe sends messages to Fred and Tina
+2.times { Message.create(sender: joe, recipient: fred, body: Faker::Lorem.sentence) }
+2.times { Message.create(sender: joe, recipient: tina, body: Faker::Lorem.sentence) }
+
+# Tina will have too many messages, based on 100 count limit
+105.times { Message.create(sender: tina, recipient: fred, body: Faker::Lorem.sentence) }
+
+3.times { Message.create(sender: fred, recipient: tina, body: Faker::Lorem.sentence) }
+
+# Fred will have some messages that are too old, based on 30 day recency limit
+first_message = Message.where(sender: fred).first
+first_message.created_at = 45.days.ago
+first_message.save
+last_message = Message.where(sender: fred).last
+last_message.created_at = 45.days.ago
+last_message.save
