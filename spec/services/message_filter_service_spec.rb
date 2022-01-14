85a0d7c79b373224463b6810c51a0ecca73b77e3
slzander <staceyzander@gmail.com>
2022-01-13 21:48:17 -0700

    Update tests for message filter service

--- spec/services/message_filter_service_spec.rb
+++ spec/services/message_filter_service_spec.rb
@@ -4,22 +4,37 @@ RSpec.describe MessageFilterService, type: :service do
   describe 'filter' do
     let(:frank_user) { User.create(name: Faker::Name.first_name, email: Faker::Internet.email) }
     let(:bob_user) { User.create(name: Faker::Name.first_name, email: Faker::Internet.email) }
+    let(:claire_user) { User.create(name: Faker::Name.first_name, email: Faker::Internet.email) }
     let(:message_from_frank_to_bob1) { Message.create(sender: frank_user, recipient: bob_user, body: Faker::Lorem.sentence) }
     let(:message_from_frank_to_bob2) { Message.create(sender: frank_user, recipient: bob_user, body: Faker::Lorem.sentence) }
     let(:message_from_frank_to_bob3) { Message.create(sender: frank_user, recipient: bob_user, body: Faker::Lorem.sentence) }
     let!(:message_from_bob_to_frank1) { Message.create(sender: bob_user, recipient: frank_user, body: Faker::Lorem.sentence) }
+    let!(:message_from_clair_to_frank1) { Message.create(sender: claire_user, recipient: frank_user, body: Faker::Lorem.sentence) }
 
-    it 'should only include records belonging to the user indicated in params, sorted from newest to oldest' do
-      message_from_frank_to_bob3.created_at = 10.days.ago
-      message_from_frank_to_bob3.save
-      message_from_frank_to_bob1.created_at = 5.days.ago
-      message_from_frank_to_bob1.save
+    context 'when there is no sender_id indicated in message params' do
+      it 'should only include all messages belonging to the user, sorted from newest to oldest' do
+        message_from_frank_to_bob3.created_at = 10.days.ago
+        message_from_frank_to_bob3.save
+        message_from_frank_to_bob1.created_at = 5.days.ago
+        message_from_frank_to_bob1.save
+        message_from_clair_to_frank1.created_at = 5.days.ago
+        message_from_clair_to_frank1.save
 
-      frank_messages = MessageFilterService.new(frank_user).filter
-      bob_messages = MessageFilterService.new(bob_user).filter
+        frank_messages = MessageFilterService.new(frank_user, {}).filter
+        bob_messages = MessageFilterService.new(bob_user, {}).filter
 
-      expect(frank_messages).to eq([message_from_frank_to_bob2, message_from_frank_to_bob1, message_from_frank_to_bob3])
-      expect(bob_messages).to eq([message_from_bob_to_frank1])
+        expect(bob_messages).to eq([message_from_frank_to_bob2, message_from_frank_to_bob1, message_from_frank_to_bob3])
+        expect(frank_messages).to eq([message_from_bob_to_frank1, message_from_clair_to_frank1])
+      end
+    end
+
+    context 'when there is a sender_id indicated in message params' do
+      let(:message_params) { { sender_id: claire_user.id } }
+      it 'should only include only messages from the sender belonging to that user' do
+        frank_messages = MessageFilterService.new(frank_user, message_params).filter
+
+        expect(frank_messages).to eq([message_from_clair_to_frank1])
+      end
     end
         
     context 'when there are records more than 30 days old' do
@@ -32,18 +47,18 @@ RSpec.describe MessageFilterService, type: :service do
         message_from_bob_to_frank3.created_at = 31.days.ago
         message_from_bob_to_frank3.save
 
-        bob_messages = MessageFilterService.new(bob_user).filter
+        frank_messages = MessageFilterService.new(frank_user, {}).filter
 
-        expect(bob_messages).to match_array([message_from_bob_to_frank1, message_from_bob_to_frank2])
+        expect(frank_messages).to match_array([message_from_bob_to_frank1, message_from_bob_to_frank2, message_from_clair_to_frank1])
       end
     end
 
     context 'when there are more than 100 records' do
       it 'should limit to 100 records' do
         105.times { Message.create(sender: bob_user, recipient: frank_user, body: Faker::Lorem.sentence) }
-        bob_messages = MessageFilterService.new(bob_user).filter
+        frank_messages = MessageFilterService.new(frank_user, {}).filter
 
-        expect(bob_messages.count).to eq(100)
+        expect(frank_messages.count).to eq(100)
       end
     end
   end
